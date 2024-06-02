9839b9242202ddbb4a662e205cb8fbb6b92813be
Anton Ignatiev <antonignatiev2005@mail.ru>
2024-06-02 22:24:25 +0300

    2 commit

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -1,9 +1,37 @@
 public class ClassesAndObjects {
+    public static void main(String[] args) {
+        Person person1;
+        person1 = new Person();
+        person1.setNameAndAge("John", 18);
+        int year1 = person1.calculateBirthday();
+        person1.speak();
+        System.out.println("Год рождения " + year1);
 
+        Person person2;
+        person2 = new Person();
+        person2.setNameAndAge("Jane", 20);
+    }
 }
-class test{
 
-}
-class test2{
+class Person {
+    String name;
+    int age;
 
+    void setNameAndAge(String username, int userage) {
+        name = username;
+        age = userage;
+    }
+
+    int calculateBirthday() {
+        int year = 2024 - age;
+        return year;
+    }
+
+    void speak() {
+        System.out.println("Меня зовут " + name + ", мне " + age + " лет");
+    }
+
+    void sayHello() {
+        System.out.println("Привет!");
+    }
 }
